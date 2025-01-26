8175f62dd9b49a00329c7bdfe8a838055c702485
PedroGama24 <126327616+PedroGama24@users.noreply.github.com>
2025-01-26 10:35:33 -0300

    Create new file header

---
+++ main.lua
@@ -0,0 +1,74 @@
+--[[
+==========================================
+Você empunha sua espada e se prepara para lutar.
+É hora de batalhar!
+
+|   Prismarine Colossus
+|
+|   Enormes estátuas de prismarina se erguem diante de você. Sua aparencia ameçadora pode assutar até os mais bravos guerreiros.
+|   Eles têm olhos turquesa brillhantes, mas um elogio não vai te ajudar a sobreviver. 
+|
+|
+|   Atributos:
+|   Vida       ▰▰▰▰▰▰▰▰▰▰
+|   Ataque     ▰▰▰▱▱▱▱▱▱▱
+|   Defesa     ▰▰▰▰▰▰▰▰▰▰  
+|   Agilidade  ▰▱▱▱▱▱▱▱▱▱
+|
+|   
+|
+|   Oque você deseja fazer?
+|   1. Atacar com a espada.
+|   2. Usar poção de regeneração.
+|   3. Atirar uma pedra.
+|   4. Se esconder.
+>
+]]
+
+-- Habilite o UTF-8
+os.execute("chcp 65001")
+
+--Header
+print([[
+====================================================
+   |^^^|                             |^^^|                            
+    }_{                               }_{
+    }_{                               }_{
+/|_/---\_|\                       /|_/---\_|\
+I _|\_/|_ I                       I _|\_/|_ I
+\| |   | |/                       \| |   | |/
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   |   |                             |   |
+   \   /                             \   /
+    \ /                               \ /
+     Y                                 Y
+      --------------------------------------
+
+             ⚔️  SIMULADOR DE BATALHA ⚔️
+
+====================================================
+
+      Você empunha sua espada e se prepara para lutar.
+                  É hora de batalhar!
+]])
+
+
+-- Obter definição do jogador
+
+-- Obter definição do mosntro
+
+-- Apresentar o mosntro
+
+
+-- Começar o loop de batalha
